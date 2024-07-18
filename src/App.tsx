8ea29bd3577e07aff4e690c07e158fa2c98fc7bd
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-07-18 14:31:36 +0100

    padding top;

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import UploadSection from './UploadSection';
 function App() {
 	return (
 		<main className='lg:p-10 px-3 lg:px-20 bg-[#edede9]/40 min-h-screen '>
-			<div className='flex flex-col gap-y-3'>
+			<div className='flex flex-col gap-y-2 pt-28 lg:pt-20'>
 				<h1 className='text-center text-5xl font-bold blur-[0.3px] select-none drop-shadow-sm cursor-pointer hover:blur-[0.35px] hover:drop-shadow-md py-3'>
 					Statement<span className='block'>Visualized</span>
 				</h1>
@@ -12,7 +12,7 @@ function App() {
 				</p>
 			</div>
 			<div className='flex gap-3 items-center justify-center py-5'>
-				<p>Powered By </p>
+				<p className='font-medium'>Powered By </p>
 				<div className='w-20 h-10 overflow-hidden'>
 					<img
 						src='/geminilogo.png'
